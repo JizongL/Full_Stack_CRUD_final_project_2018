8e12e6248e4d94ed4be65d4bcebc1239b59c34e1
tmmgeek <2015rpro@gmail.com>
2018-12-15 19:15:12 -0500

    successful routing menu.html

--- final_project.py
+++ final_project.py
@@ -10,6 +10,7 @@ restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
 restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
 
 
+
 #Fake Menu Items
 items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
 item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
@@ -17,12 +18,12 @@ item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$
 
 
 @app.route('/')
-@app.route('/restaurant')
-def showRestaurants():
-    #
-    return render_template('restaurants.html', restaurants = restaurants)
+@app.route('/restaurant/<int:restaurant_id>')
+def showRestaurants(restaurant_id):
 
-@app.route("/restaurant/new")
+    return render_template('restaurants.html', restaurants = restaurants, restaurant_id = restaurant_id)
+
+@app.route("/restaurant/new" )
 def newRestaurant():
     #
     return render_template('newRestaurant.html', restaurants = restaurants)
@@ -37,10 +38,16 @@ def deleteRestaurant():
     # return "This page will be for deleting restaurant"# %s" % restaurant_id
     return render_template('editRestaurant.html', restaurants = restaurants)
 
-@app.route("/restaurant/restaurant_id/menu")
-def showMenu():
+
+
+@app.route("/restaurant/<int:restaurant_id>/menu")
+def showMenu(restaurant_id):
+    item = {}
+    for i in items:
+        if (int(i['id'])) == (restaurant_id):
+            item = i
     # return "This page is the menu for rstaurant"#  %s" % restaurant_id|
-    return render_template('menu.html', restaurants = restaurants)
+    return render_template('menu.html', restaurants = restaurants, items = item, restaurant_id = restaurant_id)
 
 @app.route("/restaurant/restaurant_id/menu/new")
 
